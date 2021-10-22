7417afc2dc83b92ca9b8d487c95077c5770c0dab
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-22 20:30:59 -0300

    Promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertaService } from 'app/ofertas.service';
+import { Oferta } from 'app/shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertaService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: Oferta[]
+
+  constructor(private ofertasService: OfertaService) { }
 
   ngOnInit() {
+    //this.ofertas = this.ofertasService.getOfertas()
+
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      }, (param: any) => console.log(param))
+
   }
 
 }
