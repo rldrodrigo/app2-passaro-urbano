b2c561f5b3799f21215748d67d90f6e50bdeb239
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-31 06:19:57 -0300

    ç

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,5 +1,5 @@
 import { Oferta } from "./shared/oferta.model"
-import { Http } from "@angular/http"
+import { Http, Response } from "@angular/http"
 import { Injectable } from "@angular/core"
 
 import 'rxjs/add/operator/toPromise'
@@ -18,19 +18,19 @@ export class OfertaService {
     public getOfertas(): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
-            .then((resposta: any) => resposta.json())
+            .then((resposta: Response) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
-            .then((resposta: any) => resposta.json())
+            .then((resposta: Response) => resposta.json())
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
         return this.http.get(`${URL_API}/ofertas?id=${id}`)
             .toPromise()
-            .then((resposta: any) => {
+            .then((resposta: Response) => {
                 return resposta.json()[0]
             })
     }
@@ -38,7 +38,7 @@ export class OfertaService {
     public getComoUsarOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${URL_API}/como-usar?id=${id}`)
             .toPromise()
-            .then((resposta: any) => {
+            .then((resposta: Response) => {
                 return resposta.json()[0].descricao
             })
     }
@@ -46,14 +46,15 @@ export class OfertaService {
     public getOndeFicaOfertaPorId(id: number): Promise<string> {
         return this.http.get(`${URL_API}/onde-fica?id=${id}`)
             .toPromise()
-            .then((resposta: any) => {
+            .then((resposta: Response) => {
                 return resposta.json()[0].descricao
             })
     }
 
     public pesquisaOfertas(termo: string): Observable<Oferta[]> {
         return this.http.get(`${URL_API}/ofertas?descricao_oferta_like=${termo}`)
-            .retry(10)
-            .map((resposta: any) => resposta.json())
+            .retry(10) // Realiza 10 tentativas para conectar no banco
+            .map((resposta: Response) => resposta.json())
+
     }
 }
