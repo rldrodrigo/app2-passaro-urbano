c188303b3c6ba47ba937da95c352db8cb49f89ef
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-30 23:43:18 -0300

    Inserindo o camopo de pesquisa no template Topo

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertaService } from 'app/ofertas.service';
 import { Oferta } from 'app/shared/oferta.model';
-import { Observable } from 'rxjs';
-import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-oferta',
@@ -11,7 +9,8 @@ import { Observer } from 'rxjs/Observer';
   styleUrls: ['./oferta.component.css'],
   providers: [OfertaService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
 
   public oferta: Oferta
 
@@ -23,38 +22,9 @@ export class OfertaComponent implements OnInit {
       .then((oferta: Oferta) => {
         this.oferta = oferta
       })
-    /*
-  this.route.params.subscribe(
-    (parametro: any) => { console.log(parametro) },
-    (erro: any) => console.log(erro),
-    () => console.log('Processamento classificado como concluído')
-  )*/
+  }
 
-    /*
-      let tempo = Observable.interval(2000)
-  
-      tempo.subscribe((intervalo: number) => {
-        console.log(intervalo)
-      })
-  
-    */
-
-
-    //Observable (observável)
-    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
-      observer.next(1)
-      observer.next(3)
-      observer.complete()
-      //observer.error('algum erro foi encontrado na stream de eventos')
-      //Após o erro nenhum evento será disparado
-    })
-
-    //Observable (observador)
-    meuObservableTeste.subscribe(
-      (resultado: any) => console.log(resultado), //next
-      (erro: string) => console.log(erro), // error
-      () => console.log('Stream de eventos concluída') //complete
-    )
+  ngOnDestroy() {
 
   }
 
