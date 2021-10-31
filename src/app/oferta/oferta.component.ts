b0d24ca7dab03a21b52ef673bc80652cee49888c
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-30 21:21:45 -0300

    testes observable

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertaService } from 'app/ofertas.service';
 import { Oferta } from 'app/shared/oferta.model';
+import { Observable } from 'rxjs';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-oferta',
@@ -27,6 +29,33 @@ export class OfertaComponent implements OnInit {
     (erro: any) => console.log(erro),
     () => console.log('Processamento classificado como concluído')
   )*/
+
+    /*
+      let tempo = Observable.interval(2000)
+  
+      tempo.subscribe((intervalo: number) => {
+        console.log(intervalo)
+      })
+  
+    */
+
+
+    //Observable (observável)
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      observer.next(1)
+      observer.next(3)
+      observer.complete()
+      //observer.error('algum erro foi encontrado na stream de eventos')
+      //Após o erro nenhum evento será disparado
+    })
+
+    //Observable (observador)
+    meuObservableTeste.subscribe(
+      (resultado: any) => console.log(resultado), //next
+      (erro: string) => console.log(erro), // error
+      () => console.log('Stream de eventos concluída') //complete
+    )
+
   }
 
 }
