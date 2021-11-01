c7beedec6acb874d40d3a8f81145030192af993b
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-11-01 17:02:01 -0300

    Usando switchmap

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertaService } from 'app/ofertas.service';
 import { Oferta } from 'app/shared/oferta.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-topo',
@@ -12,19 +13,24 @@ import { Observable } from 'rxjs';
 export class TopoComponent implements OnInit {
 
   public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertaService: OfertaService) { }
 
   ngOnInit() {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+      .switchMap((termo: string) => {
+        console.log('requisição http para api')
+        return this.ofertaService.pesquisaOfertas(termo)
+      })
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      console.log(ofertas)
+    })
   }
 
   public pesquisa(termoDaBusca: string): void {
-    this.ofertas = this.ofertaService.pesquisaOfertas(termoDaBusca)
-
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status', erro.status),
-      () => console.log('Fluxo de eventos completo') //conclusão do subscribe
-    )
+    console.log('keyup caracter: ', termoDaBusca)
+    this.subjectPesquisa.next(termoDaBusca)
   }
 }
