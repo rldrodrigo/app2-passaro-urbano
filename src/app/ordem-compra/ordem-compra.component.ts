f76261d2ea798b0e78049073f567b0bfd5c143b3
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-11-25 11:24:46 -0300

    alteração no ordem compra bind

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = 'XYZ'
+  public numero: number = 10
+  public complemento: string = 'B'
+  public formaPagamento: string = 'debito'
+
   constructor() { }
 
   ngOnInit() {
