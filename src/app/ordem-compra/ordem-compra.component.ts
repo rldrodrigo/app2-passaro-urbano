ac9f8b5f59ad9b4538d54e373debc8539fb5815b
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-05 03:17:57 -0300

    Feedback visual na validação dos campos

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public endereco: string = 'XYZ'
-  public numero: number = 10
-  public complemento: string = 'B'
-  public formaPagamento: string = 'debito'
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
+  //controle de validação dos campos 
+  public enderecoValido: boolean = true
+  public numeroValido: boolean = true
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
 
   constructor() { }
 
@@ -19,17 +25,42 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
+
+    //Se a String for maior que 3
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true
+    } else {
+      this.enderecoValido = false
+    }
   }
 
-  public atualizaNumero(numero: number): void {
+  public atualizaNumero(numero: string): void {
     this.numero = numero
+    //Se a String for maior que 1
+    if (this.numero.length > 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
+    //Se a String for maior que 1
+    if (this.complemento.length > 1) {
+      this.complementoValido = true
+    } else {
+      this.complementoValido = false
+    }
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
+    if (
+      this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true
+    } else {
+      this.formaPagamentoValido = false
+    }
   }
 }
