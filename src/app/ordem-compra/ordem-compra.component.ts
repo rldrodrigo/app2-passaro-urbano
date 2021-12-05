5995397ed8d761713c3b3df1c1d2115ef2025711
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-05 01:13:19 -0300

    Captura de alterações no input

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -17,4 +17,19 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit() {
   }
 
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco
+  }
+
+  public atualizaNumero(numero: number): void {
+    this.numero = numero
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    this.complemento = complemento
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento
+  }
 }
