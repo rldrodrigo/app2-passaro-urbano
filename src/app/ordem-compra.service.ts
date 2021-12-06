dfe65663b1d83f978409a4daf8233cabf0bbb828
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-06 09:07:38 -0300

    Injetando o Serviço HTTP no Angular 4

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,14 @@
 import { Pedido } from "./shared/pedido.model"
+import { Http } from "@angular/http"
+import { Injectable } from "@angular/core"
 
+@Injectable()
 export class OrdemCompraService {
+
+    constructor(private http: Http) {
+
+    }
+
     public efetivarCompra(pedido: Pedido): void {
         console.log(pedido)
     }
