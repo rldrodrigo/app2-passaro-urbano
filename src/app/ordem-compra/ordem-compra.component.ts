100cbb253aa50869b595079226f0eda321e3003e
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-06 11:24:28 -0300

    Implementando TemplateFormas

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,118 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { OrdemCompraService } from 'app/ordem-compra.service';
-import { Pedido } from 'app/shared/pedido.model';
+import { OrdemCompraService } from '../ordem-compra.service'
+import { Pedido } from '../shared/pedido.model'
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OrdemCompraService]
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
-
-  public idPedidoCompra: number
-
-  //Pedido
-  public pedido: Pedido = new Pedido('', '', '', '')
-
-  public endereco: string = ''
-  public numero: string = ''
-  public complemento: string = ''
-  public formaPagamento: string = ''
-
-  //controle de validação dos campos 
-  public enderecoValido: boolean = true
-  public numeroValido: boolean = true
-  public complementoValido: boolean
-  public formaPagamentoValido: boolean
-
-  //estados primitivos dos campos (pristine)
-  public enderecoEstadoPimitivo: boolean = true
-  public numeroEstadoPimitivo: boolean = true
-  public complementoEstadoPimitivo: boolean = true
-  public formaPagamentoEstadoPimitivo: boolean = true
-
-  //Controlar botão confirmar compra
-  public formEstado: string = 'disabled'
-
-
 
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-    //this.ordemCompraService.efetivarCompra()
-  }
-
-  public atualizaEndereco(endereco: string): void {
-    this.endereco = endereco
-
-    this.enderecoEstadoPimitivo = false
-
-    //Se a String for maior que 3
-    if (this.endereco.length > 3) {
-      this.enderecoValido = true
-    } else {
-      this.enderecoValido = false
-    }
-    this.habilitaForm()
-  }
-
-  public atualizaNumero(numero: string): void {
-    this.numero = numero
-
-    this.numeroEstadoPimitivo = false
-    //Se a String for maior que 1
-    if (this.numero.length > 0) {
-      this.numeroValido = true
-    } else {
-      this.numeroValido = false
-    }
-    this.habilitaForm()
-  }
-
-  public atualizaComplemento(complemento: string): void {
-    this.complemento = complemento
-
-    this.complementoEstadoPimitivo = false
-    //Se a String for maior que 1
-    if (this.complemento.length > 1) {
-      this.complementoValido = true
-    } else {
-      this.complementoValido = false
-    }
-    this.habilitaForm()
-  }
-
-  public atualizaFormaPagamento(formaPagamento: string): void {
-    this.formaPagamento = formaPagamento
-    this.formaPagamentoEstadoPimitivo = false
-
-    if (this.formaPagamento.length > 0) {
-      this.formaPagamentoValido = true
-    } else {
-      this.formaPagamentoValido = false
-    }
-    this.habilitaForm()
-  }
-
-  public habilitaForm(): void {
-    if (this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido === true) {
-      this.formEstado = ''
-    } else {
-      this.formEstado = 'disabled'
-    }
-  }
-
-  public confirmarCompra(): void {
-
-    this.pedido.endereco = this.endereco
-    this.pedido.numero = this.numero
-    this.pedido.complemento = this.complemento
-    this.pedido.formaPagamento = this.formaPagamento
-
-    this.ordemCompraService.efetivarCompra(this.pedido)
-      .subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido
-      })
+    
   }
 }
