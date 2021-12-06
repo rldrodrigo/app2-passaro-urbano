11deef07f1e073b1fa9d3e9af044cd975fa30199
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-06 08:59:16 -0300

    Implementando o método efetivar compra

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from 'app/ordem-compra.service';
+import { Pedido } from 'app/shared/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
@@ -8,6 +9,9 @@ import { OrdemCompraService } from 'app/ordem-compra.service';
   providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
+
+  //Pedido
+  public pedido: Pedido = new Pedido('', '', '', '')
 
   public endereco: string = ''
   public numero: string = ''
@@ -95,5 +99,14 @@ export class OrdemCompraComponent implements OnInit {
     } else {
       this.formEstado = 'disabled'
     }
+  }
+
+  public confirmarCompra(): void {
+
+    this.pedido.endereco = this.endereco
+    this.pedido.numero = this.numero
+    this.pedido.complemento = this.complemento
+    this.pedido.formaPagamento = this.formaPagamento
+    this.ordemCompraService.efetivarCompra(this.pedido)
   }
 }
