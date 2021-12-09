94c8c93bd41fca780b841abc0155258416cdcd76
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-09 11:07:12 -0300

    Tratamento de envio de formulário inválido
    Manter o botão desabilitado enquanto estiver inválido, feedback visual para formulários inválidos enviados

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -27,6 +27,14 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
-    console.log(this.formulario)
+    if (this.formulario.status === 'INVALID') {
+      console.log('Formulário inválido')
+      this.formulario.get('endereco').markAsTouched()
+      this.formulario.get('numero').markAsTouched()
+      this.formulario.get('complemento').markAsTouched()
+      this.formulario.get('formaPagamento').markAsTouched()
+    } else {
+      console.log('Formulário Válido')
+    }
   }
 }
