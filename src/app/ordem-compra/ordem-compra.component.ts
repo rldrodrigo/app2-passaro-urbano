37f25484fc432dcc853453ab8a3c8c1c1e155285
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-09 11:44:05 -0300

    Tela de confirmação do pedido

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -11,6 +11,8 @@ import { Pedido } from '../shared/pedido.model'
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public idPedidoCompra: number
+
   public formulario: FormGroup = new FormGroup({
     'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
     'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(20)]),
@@ -34,7 +36,20 @@ export class OrdemCompraComponent implements OnInit {
       this.formulario.get('complemento').markAsTouched()
       this.formulario.get('formaPagamento').markAsTouched()
     } else {
-      console.log('Formulário Válido')
+
+      console.log('teste')
+      let pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      )
+
+      this.ordemCompraService.efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+          this.idPedidoCompra = idPedido
+          console.log(this.idPedidoCompra)
+        })
     }
   }
 }
