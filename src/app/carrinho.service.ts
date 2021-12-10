12dc47d944f54d69f2f10fd4729ae1f89243665a
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-10 10:10:53 -0300

    Tratamento de inclusão de itens iguais no carrinho

--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -18,8 +18,18 @@ class CarrinhoService {
             oferta.valor,
             1
         )
-        this.itens.push(itemCarrinho)
-        console.log(this.itens)
+
+        //Verificar se o item em questão já não existe dentro do this.itens
+        let itemCarrinhoEncontrado = this.itens.find((item: ItemCarrinho) => item.id === itemCarrinho.id)
+
+        if (itemCarrinhoEncontrado) {
+            itemCarrinhoEncontrado.quantidade += 1
+        } else {
+            this.itens.push(itemCarrinho)
+        }
+
+
+
     }
 
 
