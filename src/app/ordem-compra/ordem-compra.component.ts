9ae6cc554b59d2e2ed5ceac6d031592d643aa078
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-12-10 14:40:39 -0300

    Impedir compra se ordem compra estiver fazia

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -39,26 +39,29 @@ export class OrdemCompraComponent implements OnInit {
 
   public confirmarCompra(): void {
     if (this.formulario.status === 'INVALID') {
-      console.log('Formulário inválido')
+
       this.formulario.get('endereco').markAsTouched()
       this.formulario.get('numero').markAsTouched()
       this.formulario.get('complemento').markAsTouched()
       this.formulario.get('formaPagamento').markAsTouched()
     } else {
+      if (this.carrinhoService.exibirItens().length === 0) {
+        alert('Você não selecionou nenhum item!')
+      } else {
+        let pedido = new Pedido(
+          this.formulario.value.endereco,
+          this.formulario.value.numero,
+          this.formulario.value.complemento,
+          this.formulario.value.formaPagamento
+        )
 
-      console.log('teste')
-      let pedido = new Pedido(
-        this.formulario.value.endereco,
-        this.formulario.value.numero,
-        this.formulario.value.complemento,
-        this.formulario.value.formaPagamento
-      )
-
-      this.ordemCompraService.efetivarCompra(pedido)
-        .subscribe((idPedido: number) => {
-          this.idPedidoCompra = idPedido
-        })
+        this.ordemCompraService.efetivarCompra(pedido)
+          .subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido
+          })
+      }
     }
+
   }
 
   public adicionar(item: ItemCarrinho): void {
@@ -66,6 +69,7 @@ export class OrdemCompraComponent implements OnInit {
   }
 
   public decrementar(item: ItemCarrinho): void {
+
     this.carrinhoService.decrementarQuantidade(item)
   }
 }
